0bb759e1bff8de04de8c4f43685ce7fff464321f
Cristian Gutiérrez <instanciageek@gmail.com>
2021-06-11 10:57:48 -0400

    no-history: add quote model

---
+++ app/src/main/java/com/example/citas_mvvm/data/model/QuoteModel.kt
@@ -0,0 +1,8 @@
+package com.example.citas_mvvm.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class QuoteModel(
+    @SerializedName("quote") val quote: String,
+    @SerializedName("author") val author: String
+)

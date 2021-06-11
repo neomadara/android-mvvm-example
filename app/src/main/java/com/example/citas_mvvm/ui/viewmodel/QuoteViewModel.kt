cce7ead0848611f8ba3b2d39e1dfd997b2c7c042
Cristian Gutiérrez <instanciageek@gmail.com>
2021-06-11 10:59:01 -0400

    no-history: change quote view model

---
+++ app/src/main/java/com/example/citas_mvvm/ui/viewmodel/QuoteViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.citas_mvvm.ui.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.citas_mvvm.data.model.QuoteModel
+import com.example.citas_mvvm.domain.GetQuotesUseCase
+import com.example.citas_mvvm.domain.GetRandomQuoteUseCase
+import kotlinx.coroutines.launch
+
+class QuoteViewModel: ViewModel() {
+
+    val quoteModel = MutableLiveData<QuoteModel>()
+    val isLoading = MutableLiveData<Boolean>()
+
+    var getQuotesUseCase = GetQuotesUseCase()
+    var getRandomQuoteUseCase = GetRandomQuoteUseCase()
+
+    fun onCreate() {
+        // viewModelScope -> permite crear una corrutina que se controla automaticamente
+        viewModelScope.launch {
+            isLoading.postValue(true)
+            val result = getQuotesUseCase()
+
+            if(!result.isNullOrEmpty()){
+                quoteModel.postValue(result[0])
+                isLoading.postValue(false)
+            }
+        }
+    }
+
+    fun randomQuote() {
+        isLoading.postValue(true)
+        val quote = getRandomQuoteUseCase()
+        if (quote!=null){
+            quoteModel.postValue(quote)
+        }
+        isLoading.postValue(false)
+    }
+
+}
